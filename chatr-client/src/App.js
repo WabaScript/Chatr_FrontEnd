36e713f4a7a40eaa37f313f7ef319f506213e98d
Babyyodabc <rjwahba@gmail.com>
2020-04-13 11:26:02 -0400

    init

---
+++ chatr-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     Hello World
+    </div>
+  );
+}
+
+export default App;
