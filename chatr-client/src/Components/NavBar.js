4fbddb23460f6fbcaca33efa5727f659774e79c1
Babyyodabc <rjwahba@gmail.com>
2020-04-14 15:50:37 -0400

    signup and rough navlink

--- chatr-client/src/Components/NavBar.js
+++ chatr-client/src/Components/NavBar.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { ReactDOM } from 'react-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+
+const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'blue',
+    textDecoration: 'none',
+    color: 'white',
+  }
 
 const NavBar = props => {
     return (
         <div>
-            NavBar
+            <NavLink to="/" exact style={link} activeStyle={{background: "orange"}}>HomesweetHome</NavLink>
+            <NavLink to="/chats" exact style={link} activeStyle={{background: "orange"}}>Chatr Chats</NavLink>
+            <NavLink to="/popchats" exact style={link} activeStyle={{background: "orange"}}>PoPuLaR Chatr Chats</NavLink>
+            <NavLink to="/profile" exact style={link} activeStyle={{background: "orange"}}>Profile</NavLink>
+            <NavLink to="/signup" exact style={link} activeStyle={{background: "orange"}}>Become One of Us</NavLink>
         </div>
     )
 
--- chatr-client/src/Components/Signup.js
+++ chatr-client/src/Components/Signup.js
@@ -1,12 +1,44 @@
 import React from 'react'
 
-const Signup = props => {
-    return (
-        <div>
-            Signup
-        </div>
-    )
+export default class Signup extends React.Component {
 
+    state={
+        first_name: "",
+        last_name: "",
+        email: "",
+        username: "",
+        password: "",
+        pro_pic: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    handleFormReset = () => {
+        this.setState({
+            first_name: "",
+            last_name: "",
+            email: "",
+            username: "",
+            password: "",
+            pro_pic: ""
+        })
+    }
+
+    render(){
+        return(
+            <form onReset={this.handleFormReset} onSubmit={(e, signup) => this.props.handleSignupSubmit(e, this.state)}>
+                <input type="text" placeholder="First Name" name="first_name" value={this.state.first_name} onChange={this.handleChange} />  
+                <br/><input type="text" placeholder="Last Name" name="last_name" value={this.state.last_name} onChange={this.handleChange} />  
+                <br/><input type="text" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />  
+                <br/><input type="text" placeholder="Choose a Username" name="username" value={this.state.username} onChange={this.handleChange} />  
+                <br/><input type="text" placeholder="Choose a password" name="password" value={this.state.password} onChange={this.handleChange} />  
+                <br/><input type="text" placeholder="Profile Picture Url" name="pro_pic" value={this.state.pro_pic} onChange={this.handleChange} />   
+                <br/><input type="submit" value="Submit" />
+            </form>
+        )
+    }   
 }
-
-export default Signup;
