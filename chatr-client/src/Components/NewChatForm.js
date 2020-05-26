0e450c81a6a301454b6025104cbdf2fc18cef18a
Babyyodabc <rjwahba@gmail.com>
2020-05-26 18:40:23 -0400

    Rendering issues addressed

--- chatr-client/src/Components/NewChatForm.js
+++ chatr-client/src/Components/NewChatForm.js
@@ -26,7 +26,7 @@ export default class NewChatForm extends React.Component {
 
     render(){
         return(
-            <form onReset={this.handleFormReset} onSubmit={(e, chat) => this.props.handleSubmit(e, this.state)}>
+            <form  className={"padding"} onReset={this.handleFormReset} onSubmit={(e, chat) => this.props.handleSubmit(e, this.state)}>
                 <input type="text" placeholder="new topic" name="topic" value={this.state.topic} onChange={this.handleChange} />  
                 <input type="hidden" name= "owner_id"  value={this.state.owner_id}  />
                 <input type="submit" value="Submit" />
